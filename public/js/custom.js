f91af5fa1ac54c2636d4f828182245f8a804a024
Parth vora <parth.vora.777@gmail.com>
2017-07-25 19:40:41 +0530

    Remove custom.js

--- public/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('#subscription-plan').on('change', function() {
-		var name = $("#subscription-plan option:selected").data('name');
-		var amount = $("#subscription-plan option:selected").data('amount');
-		$('#stripe-js').data('name', name);
-		$('#stripe-js').data('amount', amount);
-	});
-});
